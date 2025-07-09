99f30bb97b4bface01a9b033e1a0754d748c75aa
morti3l <pushthatboard@gmail.com>
2025-07-09 00:26:09 -0500

    Feature: Add new post functionality. Add preview modal for card images. Add JS for the Save?Close buttons

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,9 @@
 const initialCards = [
     {
+        name: "Golden Gate Bridge",
+        link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/7-photo-by-griffin-wooldridge-from-pexels.jpg"
+    },
+    {
         name: "Val Thorens",
         link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/1-photo-by-moritz-feldmann-from-pexels.jpg"
     },
@@ -44,6 +48,51 @@ const newPostDescriptionInput = newPostModal.querySelector("#card-description-in
 const profileNameEl = document.querySelector(".profile__name");
 const profileDescriptionEl = document.querySelector(".profile__description");
 
+const previewModal = document.querySelector("#preview-modal");
+const previewModalCloseBtn = previewModal.querySelector(".modal__close-btn");
+const previewImageEl = previewModal.querySelector(".modal__image");
+const previewCaptionEl = previewModal.querySelector(".modal__caption");
+
+
+const cardTemplate = document
+    .querySelector("#card-template")
+    .content.querySelector(".card");
+const cardsList = document.querySelector(".cards__list");
+
+
+function getCardElement(data) {
+    const cardElement = cardTemplate.cloneNode(true);
+    const cardTitleEl = cardElement.querySelector(".card__title");
+    const cardImageEl = cardElement.querySelector(".card__image");
+
+    cardImageEl.src = data.link;
+    cardImageEl.alt = data.name;
+    cardTitleEl.textContent = data.name;
+
+    const cardLikeBtnEl = cardElement.querySelector(".card__like-btn");
+    cardLikeBtnEl.addEventListener("click", () => {
+        cardLikeBtnEl.classList.toggle("card__like-btn_active");
+    });
+
+    const cardDeleteBtnEl = cardElement.querySelector(".card__delete-btn");
+    cardDeleteBtnEl.addEventListener("click", () => {
+        cardElement.remove();
+        cardElement.null;
+    });
+
+    cardImageEl.addEventListener("click", () => {
+        previewImageEl.src = data.link;
+        previewCaptionEl.alt = data.name;
+        previewCaptionEl.textContent = data.name;
+        openModal(previewModal);
+    });
+
+    previewModalCloseBtn.addEventListener("click", () => {
+        closeModal(previewModal);
+    });
+
+    return cardElement;
+};
 
 editProfileBtn.addEventListener("click", function () {
     editProfileNameInput.value = profileNameEl.textContent;
@@ -95,14 +144,18 @@ function handleNewPostSubmit(evt) {
         return;
     }
     evt.preventDefault();
-    console.log(newPostLinkInput.value);
-    console.log(newPostDescriptionInput.value);
+    const inputValues = {
+        name: newPostLinkInput.value,
+        link: newPostDescriptionInput.value
+    };
+    const postCardEl = getCardElement(inputValues);
+    cardsList.prepend(postCardEl);
     newPostModal.classList.remove("modal_is-opened");
     newPostForm.reset();
 
 };
 
 initialCards.forEach(function (item) {
-    console.log(item.name);
-    console.log(item.link);
+    const postCardEl = getCardElement(item);
+    cardsList.append(postCardEl);
 });
