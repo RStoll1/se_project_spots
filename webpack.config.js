c573d83ddd1af7f06bbd0bc5d04a6eebe8fbf385
RyanStoll <pushthatboard@gmail.com>
2025-09-18 21:33:41 -0500

    Add edit profile button and stylings.

--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,72 @@
-const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-module.exports = {
-    entry: './src/pages/index.js',
+module.exports = (env, argv) => {
+  const isDev = argv.mode === "development";
+
+  return {
+    entry: {
+      main: "./src/pages/index.js",
+    },
     output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'main.js',
+      path: path.resolve(__dirname, "dist"),
+      filename: "main.js",
+      publicPath: "",
+      clean: true, // ensures output folder is cleaned
     },
-    mode: 'development',
-    devtool: 'inline-source-map',
+    mode: isDev ? "development" : "production",
+    devtool: isDev ? "inline-source-map" : false,
+    stats: "errors-only",
     devServer: {
-        static: path.resolve(__dirname, 'dist'),
-        compress: true,
-        port: 8080,
-        open: true,
-        hot: true,
-        historyApiFallback: true,
+      static: path.resolve(__dirname, "dist"),
+      compress: true,
+      port: 8080,
+      open: true,
+      liveReload: true,
+      hot: false,
     },
+    target: ["web", "es5"],
     module: {
-        rules: [
-            {
-                test: /\.js$/,
-                use: 'babel-loader',
-                exclude: /node_modules/,
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
             },
+          },
+        },
+        {
+          test: /\.css$/,
+          use: [
+            isDev ? "style-loader" : MiniCssExtractPlugin.loader,
             {
-                test: /\.css$/,
-                use: [
-                    process.env.NODE_ENV === 'production'
-                        ? MiniCssExtractPlugin.loader
-                        : 'style-loader', // 'style-loader' for dev
-                    'css-loader',
-                    'postcss-loader',
-                ],
+              loader: "css-loader",
+              options: {
+                importLoaders: 1,
+              },
             },
-        ],
+            "postcss-loader",
+          ],
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|webp|gif|woff(2)?|eot|ttf|otf)$/,
+          type: "asset/resource",
+        },
+      ],
     },
     plugins: [
-        new MiniCssExtractPlugin({
-            filename: '[name].[contenthash].css',
-        }),
+      new HtmlWebpackPlugin({
+        template: "./src/index.html",
+      }),
+      new CleanWebpackPlugin(),
+      new MiniCssExtractPlugin({
+        filename: "styles.css",
+      }),
     ],
+  };
 };
